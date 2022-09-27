d5f5f691b430b35227421af49f4644122c2a5f59
obaida <obaidashurbaji99@gmail.com>
2022-09-27 19:03:01 +0200

    setting nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-// const Nav = () => {
-// 	return (
-// 		<nav>
-// 			<img src="../assets/logo.png" alt="logo" />
-// 			<ul>
-// 				<li>
-// 					<Link to="/">Home</Link>
-// 				</li>
-// 				<li>
-// 					<Link to="">Applications</Link>
-// 				</li>
-// 			</ul>
-// 		</nav>
-// 	);
-// };
-
-// export default Nav;
-
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
