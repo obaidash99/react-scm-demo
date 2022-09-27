a07b5ec6042939f7bdadd7340a538b78cf38617e
obaida <obaidashurbaji99@gmail.com>
2022-09-27 19:02:26 +0200

    setting some things up

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,128 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-const Nav = () => {
-	return (
-		<nav>
-			<img src="../assets/logo.png" alt="logo" />
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="">Applications</Link>
-				</li>
-			</ul>
-		</nav>
+// const Nav = () => {
+// 	return (
+// 		<nav>
+// 			<img src="../assets/logo.png" alt="logo" />
+// 			<ul>
+// 				<li>
+// 					<Link to="/">Home</Link>
+// 				</li>
+// 				<li>
+// 					<Link to="">Applications</Link>
+// 				</li>
+// 			</ul>
+// 		</nav>
+// 	);
+// };
+
+// export default Nav;
+
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import logo from '../assets/logo.png';
+
+const drawerWidth = 240;
+const navItems = ['Home', 'Applications'];
+
+function Nav(props) {
+	const { window } = props;
+	const [mobileOpen, setMobileOpen] = React.useState(false);
+
+	const handleDrawerToggle = () => {
+		setMobileOpen(!mobileOpen);
+	};
+
+	const drawer = (
+		<Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+			<Typography variant="h6" sx={{ my: 2 }}>
+				<img src={logo} alt="Logo" className="logo" />
+			</Typography>
+			<Divider />
+			<List>
+				{navItems.map((item) => (
+					<ListItem key={item} disablePadding>
+						<ListItemButton sx={{ textAlign: 'center' }}>
+							<ListItemText primary={item} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
+		</Box>
 	);
+
+	const container = window !== undefined ? () => window().document.body : undefined;
+
+	return (
+		<Box sx={{ display: 'flex' }}>
+			<AppBar component="nav">
+				<Toolbar>
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						edge="start"
+						onClick={handleDrawerToggle}
+						sx={{ mr: 2, display: { sm: 'none' } }}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography
+						variant="h6"
+						component="div"
+						sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+					>
+						<img src={logo} alt="Logo" className="logo" />
+					</Typography>
+					<Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+						{navItems.map((item) => (
+							<Button key={item} sx={{ color: '#fff' }}>
+								{item}
+							</Button>
+						))}
+					</Box>
+				</Toolbar>
+			</AppBar>
+			<Box component="nav">
+				<Drawer
+					container={container}
+					variant="temporary"
+					open={mobileOpen}
+					onClose={handleDrawerToggle}
+					ModalProps={{
+						keepMounted: true, // Better open performance on mobile.
+					}}
+					sx={{
+						display: { xs: 'block', sm: 'none' },
+						'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+					}}
+				>
+					{drawer}
+				</Drawer>
+			</Box>
+		</Box>
+	);
+}
+
+Nav.propTypes = {
+	/**
+	 * Injected by the documentation to work in an iframe.
+	 * You won't need it on your project.
+	 */
+	window: PropTypes.func,
 };
 
 export default Nav;
