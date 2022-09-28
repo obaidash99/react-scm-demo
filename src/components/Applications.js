36b68d5a60f34fba6eb2c642eabafce0a27ffe54
obaida <obaidashurbaji99@gmail.com>
2022-09-28 12:41:12 +0200

    starting with apps

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Applications = () => {
-	return <div>Here is the list of the applications</div>;
+	return (
+		<div className="applications">
+			<div className="container">
+				<div className="content">
+					<div className="card">
+						<h4>VMware - في ام وير</h4>
+						<p>نظام الويندوز الافتراضي باستخدام - الفي ام وير</p>
+					</div>
+					<div className="card">
+						<h4>VMware - في ام وير</h4>
+						<p>نظام الويندوز الافتراضي باستخدام - الفي ام وير</p>
+					</div>
+					<div className="card">
+						<h4>VMware - في ام وير</h4>
+						<p>نظام الويندوز الافتراضي باستخدام - الفي ام وير</p>
+					</div>
+					<div className="card">
+						<h4>VMware - في ام وير</h4>
+						<p>نظام الويندوز الافتراضي باستخدام - الفي ام وير</p>
+					</div>
+					<div className="card">
+						<h4>VMware - في ام وير</h4>
+						<p>نظام الويندوز الافتراضي باستخدام - الفي ام وير</p>
+					</div>
+					<div className="card">
+						<h4>VMware - في ام وير</h4>
+						<p>نظام الويندوز الافتراضي باستخدام - الفي ام وير</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Applications;
