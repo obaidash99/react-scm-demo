44ab8e83b049a8b9aeb75c92ad4f29fdf257b683
obaida <obaidashurbaji99@gmail.com>
2022-10-03 22:08:48 +0200

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import footerImg from '../assets/veraCryptImgs/image13.jpg';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
 	return (
 		<footer>
-				<div className="footer">
-					<div className="image">
-						<a href="https://scm.bz/" target="_blank" rel="noreferrer">
-							<img src={footerImg} alt="footer-img" />
-						</a>
-					</div>
-					<div className="footer-content">
-						<ul>
-							<li>
-								<a href="#home">الصفحة الرئيسية</a>
-							</li>
-							<li>
-								<a href="#applications">التطبيقات</a>
-							</li>
-						</ul>
-					</div>
+			<div className="footer">
+				<div className="image">
+					<a href="https://scm.bz/" target="_blank" rel="noreferrer">
+						<img src={footerImg} alt="footer-img" />
+					</a>
 				</div>
+				<div className="footer-content">
+					<ul>
+						<li>
+							<Link to="/">الصفحة الرئيسية</Link>
+						</li>
+						<li>
+							<Link to="/applications">التطبيقات</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
 			<div className="copy-right">
 				<p className="copy">Copyright SCM &copy; 2022</p>
 			</div>
