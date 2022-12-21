fa83b054e0a73a3a05ebc286a79af202cbac2a0b
obaida <obaidashurbaji99@gmail.com>
2022-12-21 23:07:14 +0200

    some style fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Applications from './Applications';
 
 const Home = () => {
 	return (
